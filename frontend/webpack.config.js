71624bfad51163cc63861680a04d46b2ba5d6678
Tara Ojo <tara.ojo@futurelearn.com>
2017-10-21 11:08:32 +0100

    Set up foundation front end

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: 'build.js'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
